d0eb77c24eab70369afb5fb2acfd8b32ec12bf84
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-10-23 11:37:21 +0300

    Add DataBase

---
+++ Voting mobile application/Assets/Scripts/SelectCity.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using UnityEngine.UI;
+using System.IO;
+
+public class SelectCity : MonoBehaviour
+{
+    public Dropdown selectCity;
+    public Text cityName;
+    private string[] textValue;
+    UserState userState;
+    void Update()
+    {
+        if (SelectCountry.isSelected)
+        {
+            FromJson();
+            DBSelect();
+            SelectCountry.isSelected = false;
+        }
+    }
+
+    public void DBSelect()
+    {
+        string connectionString = "URI=file:" + Application.dataPath + "/DataBases/country.sqlite";
+        using (IDbConnection dbcon = (IDbConnection)new SqliteConnection(connectionString))
+        {
+            dbcon.Open();
+            var sql = "SELECT * FROM cities WHERE state_id=" + userState.id;
+            using (IDbCommand dbcmd = dbcon.CreateCommand())
+            {
+                dbcmd.CommandText = sql;
+                using (IDataReader reader = dbcmd.ExecuteReader())
+                {
+                    selectCity.ClearOptions();
+                    while (reader.Read())
+                    {
+                        selectCity.options.Add(new Dropdown.OptionData() { text = reader.GetString(1) });
+                        // "id: {0}; sortname: {1}; name: {2}; phonecode: {3};";
+                    }
+                }
+            }
+            dbcon.Close();
+        }
+    }
+
+    public void Selected()
+    {
+
+    }
+
+    public void FromJson()
+    {
+        string path = Application.dataPath + "/DataBases/data.json";
+        string json = File.ReadAllText(path);
+        userState = JsonUtility.FromJson<UserState>(json);
+        Debug.Log(userState.id);
+    }
+
+    public void SaveToJson()
+    {
+        textValue = cityName.text.Split('.');
+        userState = new UserState()
+        {
+            city = textValue[1].Trim()
+        };
+        string potion = JsonUtility.ToJson(userState);
+        System.IO.File.WriteAllText(Application.dataPath + "/DataBases/data.json", potion);
+    }
+
+   // public void Selected()
+  //  {
+  //      if (selectCity.value > 0)
+  //      {
+  //          SaveToJson();
+  //      }
+  //  }
+
+    [System.Serializable]
+    public class UserState
+    {
+        public string id;
+        public string country;
+        public string city;
+    }
+}
---
+++ Voting mobile application/Assets/Scripts/SelectCountry.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using UnityEngine.UI;
+
+public class SelectCountry : MonoBehaviour
+{
+    public Dropdown selectCountries;
+    public Text countryName;
+    private UserState userState;
+    private string[] textValue;
+    public static bool isSelected = false;
+    void Start()
+    {
+        string connectionString = "URI=file:" + Application.dataPath + "/DataBases/country.sqlite";
+        using (IDbConnection dbcon = (IDbConnection)new SqliteConnection(connectionString))
+        {
+            dbcon.Open();
+            var sql = "SELECT * FROM countries";
+            using (IDbCommand dbcmd = dbcon.CreateCommand())
+            {
+                dbcmd.CommandText = sql;
+                using (IDataReader reader = dbcmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        selectCountries.options.Add(new Dropdown.OptionData() { text = reader.GetInt32(0) + ". " + reader.GetString(2) });
+                        // "id: {0}; sortname: {1}; name: {2}; phonecode: {3};";
+                    }
+                }
+            }
+            dbcon.Close();
+        }
+    }
+
+    public void SaveToJson()
+    {
+        Debug.Log(countryName);
+        textValue = countryName.text.Split('.');
+        userState = new UserState()
+        {
+            id = textValue[0],
+            country = textValue[1].Trim()
+        };
+        string potion = JsonUtility.ToJson(userState);
+        System.IO.File.WriteAllText(Application.dataPath + "/DataBases/data.json", potion);
+    }
+
+    public void Selected()
+    {
+        if (selectCountries.value > 0)
+        {
+            SaveToJson();
+            isSelected = true;
+        }
+    }
+
+    [System.Serializable]
+    public class UserState
+    {
+        public string id;
+        public string country;
+        public string city;
+    }
+}
