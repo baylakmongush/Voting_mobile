120c40387f8e1d32bc2f0877c913c0f35f2acd83
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-10-26 23:53:00 +0300

    Add Stat

--- Voting mobile application/Assets/Scripts/ButtonMovement.cs
+++ Voting mobile application/Assets/Scripts/ButtonMovement.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class ButtonMovement : MonoBehaviour
 {
     const float MinImpulseForce = 1f;
-    const float MaxImpulseForce = 2f;
+    const float MaxImpulseForce = 3f;
+    bool istrue = true;
+    Vector3 OldPosition;
+    Rigidbody2D rb;
+    float angle;
+    Vector2 direction;
+    float waitedMove = 0;
+    bool movingBall = false;
 
     void Start()
     {
-        Movement();
+           Movement();
+        //   OldPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
+    /*void Update()
+    {
+        if (OldPosition == transform.position && istrue)
+        {
+            Movement();
+            istrue = false;
+        }
+        else
+            istrue = true;
+        OldPosition = transform.position;
+    }*/
+    //Работает безотказно
 
     // Update is called once per frame
     void Movement()
@@ -20,4 +41,24 @@ public class ButtonMovement : MonoBehaviour
         float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
         GetComponent<Rigidbody2D>().AddForce(moveDirection * 1.3f, ForceMode2D.Impulse);
     }
+    /*    void Update()
+        {
+            //move ball
+            waitedMove += Time.deltaTime;
+            angle = -90 * Mathf.Deg2Rad;
+            bool moveBool = waitedMove >= 1f;
+            if (moveBool && !movingBall)
+            {
+                direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                rb.AddForce(direction * 60f);
+                movingBall = true;
+            }
+        }
+
+            public void SetDirection(Vector2 direction)
+        {
+            Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+            rb2d.velocity = rb2d.velocity.magnitude * direction;
+        }*/
+
 }
---
+++ Voting mobile application/Assets/Scripts/ScreenFrame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFrame : MonoBehaviour
+{
+	const float BounceAngleHalfRange = 60f;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+/*	void OnCollisionEnter2D(Collision2D coll)
+	{
+		bool ballTop = coll.gameObject.GetComponent<ButtonMovement>().transform.position.y > transform.position.y - 0.05f;
+			// calculate new ball direction
+			if (ballTop)
+			{
+				float ballOffsetFromPaddleCenter = transform.position.x -
+					coll.transform.position.x;
+				float normalizedBallOffset = ballOffsetFromPaddleCenter / 5;
+				float angleOffset = normalizedBallOffset * BounceAngleHalfRange;
+				float angle = Mathf.PI / 2 + angleOffset;
+				Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+				// tell ball to set direction to new direction
+				ButtonMovement ballScript = coll.gameObject.GetComponent<ButtonMovement>();
+				ballScript.SetDirection(direction);
+			}
+	}*/
+}
