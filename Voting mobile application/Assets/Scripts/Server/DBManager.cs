8fe4259abdb72e9587d0bf3de16316ede0d127b2
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-10-28 11:58:26 +0300

    add some changes

--- Voting mobile application/Assets/Scripts/Server/DBManager.cs
+++ Voting mobile application/Assets/Scripts/Server/DBManager.cs
@@ -1,18 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.IO;
 
 public class DBManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // MySQL настройки 
+    string constr = "Server=localhost;Database=voting;User ID=root;Password=Theek7Ee;Pooling=true;CharSet=utf8;";
+	// соединение 
+	MySqlConnection con = null;
+	// команда к БД
+	MySqlCommand cmd = null;
+	// чтение
+	MySqlDataReader rdr = null;
+	// ошибки
+	MySqlError er = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// массив синхронизируемых игровых объектов
+	GameObject[] bodies;
+
+	// Названия синхронизируемых полей таблицы в БД
+	string ID, Name, levelname, objectType;
+	float posx, posy, posz, tranx, trany, tranz;
+	// описание структуры синхронизируемых данных
+	public struct data
+	{
+		public int UID;
+		public string ID, Name, levelname, objectType;
+		public float posx, posy, posz, tranx, trany, tranz;
+	}
+	// коллекция в которую будут записываться данные о синхронизируемых объектах
+	List<data> _GameItems;
+	void Awake()
+	{
+		try
+		{
+			// установка элемента соединения 
+			con = new MySqlConnection(constr);
+
+			// посмотрим, сможем ли мы установить соединение 
+			con.Open();
+			Debug.Log("Connection State: " + con.State);
+		}
+		catch (IOException ex) { Debug.Log(ex.ToString()); }
+	}
+
+	void OnApplicationQuit()
+	{
+		Debug.Log("killing con");
+		if (con != null)
+		{
+			// Конечно, правильнее использовать:
+			// if (con.State != ConnectionState.Closed) 
+			// но из-за проблем с версиями сборок приходится использовать костыли
+			if (con.State.ToString() != "Closed")
+				con.Close();
+			con.Dispose();
+		}
+	}
 }
---
+++ Voting mobile application/Assets/Scripts/Server/Request.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Request : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        StartCoroutine(GetRequest("http://127.0.0.1:5000/"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
