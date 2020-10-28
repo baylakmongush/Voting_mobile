4d61a3a07540a340202258e1dc57a3583449c3a8
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-10-28 15:46:47 +0300

    аа

--- Voting mobile application/Assets/Scripts/Server/DBManager.cs
+++ Voting mobile application/Assets/Scripts/Server/DBManager.cs
@@ -7,57 +7,31 @@ using System.IO;
 
 public class DBManager : MonoBehaviour
 {
-    // MySQL настройки 
-    string constr = "Server=localhost;Database=voting;User ID=root;Password=Theek7Ee;Pooling=true;CharSet=utf8;";
-	// соединение 
-	MySqlConnection con = null;
-	// команда к БД
-	MySqlCommand cmd = null;
-	// чтение
-	MySqlDataReader rdr = null;
-	// ошибки
-	MySqlError er = null;
+    void Start()
+    {
+        string cs = @"server=127.0.0.1;userid=root;
+           password=adventuretimes!12;database=voting;Pooling=true;CharSet=utf8;";
 
-	// массив синхронизируемых игровых объектов
-	GameObject[] bodies;
+        MySqlConnection conn = null;
 
-	// Названия синхронизируемых полей таблицы в БД
-	string ID, Name, levelname, objectType;
-	float posx, posy, posz, tranx, trany, tranz;
-	// описание структуры синхронизируемых данных
-	public struct data
-	{
-		public int UID;
-		public string ID, Name, levelname, objectType;
-		public float posx, posy, posz, tranx, trany, tranz;
-	}
-	// коллекция в которую будут записываться данные о синхронизируемых объектах
-	List<data> _GameItems;
-	void Awake()
-	{
-		try
-		{
-			// установка элемента соединения 
-			con = new MySqlConnection(constr);
+        try
+        {
+            conn = new MySqlConnection(cs);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM `valuesvoting`", conn);
+            var result = cmd.ExecuteReader();
+            while (result.Read())
+            {
+                int value = result.GetInt32(0);
+                string name = result.GetString(1);
+                string rand = result.GetString(2);
 
-			// посмотрим, сможем ли мы установить соединение 
-			con.Open();
-			Debug.Log("Connection State: " + con.State);
-		}
-		catch (IOException ex) { Debug.Log(ex.ToString()); }
-	}
-
-	void OnApplicationQuit()
-	{
-		Debug.Log("killing con");
-		if (con != null)
-		{
-			// Конечно, правильнее использовать:
-			// if (con.State != ConnectionState.Closed) 
-			// но из-за проблем с версиями сборок приходится использовать костыли
-			if (con.State.ToString() != "Closed")
-				con.Close();
-			con.Dispose();
-		}
-	}
+                Debug.Log("value= " + value + "  name =" + name + "  random =" + rand);
+            }
+        }
+        catch (MySqlException ex)
+        {
+            Debug.Log("Error: {0}" + ex.ToString());
+        }
+    }
 }
