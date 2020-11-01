a937389e0f85a23f7e0af641d25f610a6ab483b6
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-11-01 10:56:18 +0300

    Add in server json to mysql

--- Voting mobile application/Assets/Scripts/Button2Value.cs
+++ Voting mobile application/Assets/Scripts/Button2Value.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
 
 public class Button2Value : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text content;
+    public void Click()
     {
-        
+        string json = File.ReadAllText(Application.dataPath + "/DataBases/data.json");
+        Users potion = JsonUtility.FromJson<Users>(json);
+        Debug.Log(potion.age + " = age");
+        potion.vote = content.text;
+        string newjson = JsonUtility.ToJson(potion);
+        System.IO.File.WriteAllText(Application.dataPath + "/DataBases/data.json", newjson);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class Users
     {
-        
+        public string id;
+        public string country;
+        public string city;
+        public string age;
+        public string vote;
     }
 }
---
+++ Voting mobile application/Assets/Scripts/Server/PostData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.IO;
+using System.Text;
+
+public class PostData : MonoBehaviour
+{
+	// Start is called before the first frame update
+	string data;
+	public Text value1;
+	public Text value2;
+	void Start()
+	{
+		//DontDestroyOnLoad(this);
+		StartCoroutine(SetRequest("http://127.0.0.1:5000/data"));
+	}
+
+	IEnumerator SetRequest(string uri)
+	{
+		string json = File.ReadAllText(Application.dataPath + "/DataBases/data.json");
+		var request = new UnityWebRequest(uri, "POST");
+		byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+		request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+		request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+		request.SetRequestHeader("Content-Type", "application/json");
+		yield return request.SendWebRequest();
+
+		if (request.error != null)
+		{
+			Debug.Log("Erro: " + request.error);
+		}
+		else
+		{
+			Debug.Log("All OK");
+			Debug.Log("Status Code: " + request.responseCode);
+		}
+	}
+}
