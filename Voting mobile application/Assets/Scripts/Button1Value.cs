0c090678fcb95ea28cf6a46842b59f3a7a26a31f
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-11-01 05:52:46 +0300

    Add to json vote

---
+++ Voting mobile application/Assets/Scripts/Button1Value.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class Button1Value : MonoBehaviour
+{
+    public Text content;
+    public void Click()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/DataBases/data.json");
+        Users potion = JsonUtility.FromJson<Users>(json);
+        Debug.Log(potion.age + " = age");
+        potion.vote = content.text;
+        string newjson = JsonUtility.ToJson(potion);
+        System.IO.File.WriteAllText(Application.dataPath + "/DataBases/data.json", newjson);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    [System.Serializable]
+    public class Users
+    {
+        public string id;
+        public string country;
+        public string city;
+        public string age;
+        public string vote;
+    }
+}
