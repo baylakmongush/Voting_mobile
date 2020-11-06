af8a5de28b9f92285ab801b903008b1fb1b9fb55
Baylak Mongush <56039676+npetrelli@users.noreply.github.com>
2020-11-06 21:24:05 +0300

    Add new location opr

--- Voting mobile application/Assets/Scenes/TestLocationService.cs
+++ Voting mobile application/Assets/Scenes/TestLocationService.cs
@@ -1,45 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
 
 public class TestLocationService : MonoBehaviour
 {
-    IEnumerator Start()
+    private void Start()
     {
-        // First, check if user has location service enabled
-        if (!Input.location.isEnabledByUser)
-            yield break;
+        StartCoroutine("DetectCountry");
+    }
 
-        // Start service before querying location
-        Input.location.Start();
-
-        // Wait until service initializes
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+    [System.Obsolete]
+    IEnumerator DetectCountry()
+    {
+        UnityWebRequest request = UnityWebRequest.Get("https://extreme-ip-lookup.com/json");
+        request.chunkedTransfer = false;
+        yield return request.Send();
+        if (request.isError)
         {
-            yield return new WaitForSeconds(1);
-            maxWait--;
-        }
-
-        // Service didn't initialize in 20 seconds
-        if (maxWait < 1)
-        {
-            Debug.Log("Timed out");
-            yield break;
-        }
-
-        // Connection has failed
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            Debug.Log("Unable to determine device location");
-            yield break;
+            Debug.Log("Error");
         }
         else
         {
-            // Access granted and location value could be retrieved
-            Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            if (request.isDone)
+            {
+                Country res = JsonUtility.FromJson<Country>(request.downloadHandler.text);
+                Debug.Log(res.country);
+                Debug.Log(res.countryCode);
+                Debug.Log(res.city);
+            }
         }
+    }
 
-        // Stop service if there is no need to query location updates continuously
-        Input.location.Stop();
+    public class Country
+    {
+        public string businessName;
+        public string businessWebsite;
+        public string city;
+        public string continent;
+        public string country;
+        public string countryCode;
+        public string ipName;
+        public string ipType;
+        public string isp;
+        public string lat;
+        public string lon;
+        public string org;
+        public string query;
+        public string region;
+        public string status;
     }
 }
